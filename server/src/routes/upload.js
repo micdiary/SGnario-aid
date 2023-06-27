b7876f373de4489137ea929f59b1d342d41e8a21
2102673 <2102673@sit.singaporetech.edu.sg>
2023-06-28 03:12:06 +0800

    Edited deleteFolder function to delete to work for both fil and folder deletion

--- server/src/routes/upload.js
+++ server/src/routes/upload.js
@@ -11,8 +11,8 @@ const upload = multer({ dest: "uploads/" });
 
 const getDrive = async () => {
     const credentials = {
-        client_email: "",
-        private_key: "",
+        client_email: "sgnario-aid@sgnario-aid.iam.gserviceaccount.com",
+        private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCnzWyCX75JVaP5\nWh4tzgwq4M+ltZrLUruW/KrTvbQKtgfE8ujoAiIueSz9GrlHu3W70vikNL914gev\niOqNisQNGCR5TYk2JBqIgZf6Giphna1pOK+QBYEPkNl120YzmbCqiWSxiLQdPA2v\nr/UAVxFN9E5ok/XR5kogKeNpZ4DOAQ4fCHEVGz1AkFt3/eFPEliBHW9mLqP/xwnj\nvGz3xxnlP1KO5Kj87nBecCvuFMlptWoZsLBAcgkM9XJvSHLIctdG7d3RlH45yE5+\nWnWb+Nng97lQ65Vyb/8mz3G5MALlJXn3uHVXCc1ytJM6hVcuztSn0M9Qkai0+ALe\nUEg8IQunAgMBAAECggEABsIbxi5z/AEifwb1ozOiwtFWG57yUpEM9BpOCQQhbbMC\nIhkEb1+yn7uKp9D9OFng9oUbNYXYVjeZxqSVicjWd46K3YXVamRO20OY6t5F6bOP\niebaD0thdM6JK5l+vNJKTrwE3a+bTXgfNv23uOFI1aJqzAR1iJQlVSFKwvKDugg5\nHI9vR1nEDkgM+KoaPjuJki6afUc+yWdGsSvztJdhW6Zv4amkhCSrAMWSXkgY7gyU\nDcuQHdVzpAk4JOj9DD+oM4Dcz3q/7EjIsvWVwSAfjYbbZ0PYo02G7ECwQXPmPiUS\nCZf9cBEaQhYnDa1A9gKoi7VZDG8cLZDruwT0mO0zpQKBgQDeMmql3QCoH/GrcIeE\n1bbeNzTqpdCclnZ9fMfvUL02SregSa54ZTRaxGC48omd0PRqmH/ResNq/354DqPE\nL17v1L1F2eemdMGpFwJEBd7tggmTazqW7tSJpcub5hiZR1xdoNWnF30K7qZ8jjpR\nTms5InAU4F/jlVmsKsKrs7udPQKBgQDBVJVGRPNPulXsCzzbNHf2deyG5YoajVbw\nvcs7/MSMKnrweoEYgwzyTdIKyHuVTKp8oauqaJ2MyjeUMCLqg3pgeLr3FGC3eCSR\ni8jTGXvEWUPkax9srZZL90f381+Z6YC9a6/ur2vyQ514ywIr6zESP1B1Nb5kT/Ud\nPeN7qlAiswKBgQDEQqX+sTGmTujotDGPfDHVH9BGvoV/+krY1oHG+XrXXLHLC6nm\nwAW5cqG4WsqBMhLhoKetwRChDuGHa+7tF6Z0kmVIMIuzdIemMiOXL9RT1ztcEiej\nL3tdxasNnfT2VpjYPnrY5x9Uq600cXBDZs/AnJGBbxpjbATT1MU0YkICRQKBgQCe\nXiBNyKubcuNxkZ8PXcKrRNw1gJCJwaAnYwG5qQyEWUQN+/wGTUvsrOzTYnlByuSZ\nZYIO/NWagAfZPzlK69rybdOSFMJJaIRkKaKfsm9dX1YrJck/KE9G+VCd/2If3VRV\nVoJuvoppkRPM4FduDKJM9Up+SUEmQQ1kH89Z/SDZ2wKBgQDSrfH5Jq5Jt9LEPFKN\nL7gtOYuhBLRewMTpw/jPpXS8Rg1FWDzeV50zs/S/AUUNuNPW4z1CTLM3Q8YpGz7p\nLpXZPCyAythCMFuhwajAeJdEIR9wEoXFvUp+ofC8eJDgtBKDmzvtlGaUpzXrPs6j\nt96gAAh5L200a8XEaF+FFw1WbA==\n-----END PRIVATE KEY-----\n",
     };
     // Authenticate with the Google Drive API using credentials from the credentials.json file
     const auth = new google.auth.GoogleAuth({
@@ -140,23 +140,24 @@ router.post("/createFolder", async (req, res) => {
     }
 });
 
-router.post("/deleteFolder", async (req, res) => {
+router.post("/delete", async (req, res) => {
     try {
-        const { folderId } = req.body;
+        const { fileId } = req.body;
 
         const drive = await getDrive();
-        // Delete the folder
+        // Delete the folder/file
         await drive.files.delete({
-            fileId: folderId,
+            fileId: fileId,
         });
-        console.log("Folder deleted successfully");
+        console.log( fileId + " deleted successfully");
         res.status(200).json({
-            message: "Folder deleted successfully",
+            message: fileId + " deleted successfully",
         });
     } catch (error) {
-        console.error("Error deleting folder:", error);
-        res.status(500).json({ message: "Failed to delete folder" });
+        console.error("Error deleting file:", error);
+        res.status(500).json({ message: "Failed to delete file"});
     }
 });
+
 
 export { router as uploadRouter };
